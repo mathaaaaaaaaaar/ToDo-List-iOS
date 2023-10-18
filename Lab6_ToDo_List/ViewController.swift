f37705aad78999183e9f56e5a014c566b72f0052
Shaik Mathar Syed <shaik.mathar99@gmail.com>
2023-10-17 21:14:49 -0400

    set up project

---
+++ Lab6_ToDo_List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab6_ToDo_List
+//
+//  Created by Shaik Mathar Syed on 17/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
